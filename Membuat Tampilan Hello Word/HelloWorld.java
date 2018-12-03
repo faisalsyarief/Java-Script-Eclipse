36dc6e7ba091ccc6e61f93d1162ad13b04628f16
faisal <ichaltkj96@gmail.com>
2018-12-03 14:44:28 +0700

    java

---
+++ Membuat Tampilan Hello Word/HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+*/
+class HelloWorld{
+//D4 Teknik Informatika 1B()
+public static void main(String[] f) 
+	{System.out.println("       HelloWorld");
+	System.out.println("           )))");
+	System.out.println("          (. .)");
+	System.out.println("         (( v ))");
+	System.out.println("        ---m-m---");
+	System.out.println("   Faisal Syarifuddin");
+	System.out.println("D4 Teknik Informatika 1B");
+}
+}
